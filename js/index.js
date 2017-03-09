adb40825e661a370d13ea2b434621196f4218999
Meghan Hamannwright <meghankh@vt.edu>
2017-03-08 21:19:00 -0700

    added full meter and base baby on it

--- js/index.js
+++ js/index.js
@@ -1,30 +1,84 @@
 
 jQuery('document').ready(function() {
-    jQuery('#baby1').click(function() {
-        jQuery('#baby1').hide();
-        jQuery('#baby2').show();
-        jQuery('#meter1').hide();
-        jQuery('#meter2').show();
-    });
-    jQuery('#baby2').click(function() {
-        jQuery('#baby2').hide();
-        jQuery('#baby3').show();
-        jQuery('#meter2').hide();
-        jQuery('#meter3').show();
-    });
-    jQuery('#baby3').click(function() {
-        jQuery('#baby3').hide();
-        jQuery('#baby4').show();
-        jQuery('#meter3').hide();
-        jQuery('#meter4').show();
-    });
-    jQuery('#baby4').click(function() {
-        jQuery('#baby4').hide();
-        jQuery('#baby1').show();
-        jQuery('#meter4').hide();
-        jQuery('#meter1').show();
-    });
 
+    var currentPosition = 0;
+
+    function increase() {
+        jQuery('#meter' + currentPosition).hide();
+        if (currentPosition < 12)
+        {
+            currentPosition++;
+        }
+        if (currentPosition < 4)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby1').show();
+        }
+        if (currentPosition < 7 && currentPosition > 3)
+        {
+            jQuery('#baby1').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby2').show();
+        }
+        if (currentPosition < 10 && currentPosition > 6)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby1').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby3').show();
+        }
+        if (currentPosition > 9)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby1').hide();
+            jQuery('#baby4').show();
+        }
+        jQuery('#meter' + currentPosition).show();
+    }
+
+    function decrease() {
+        jQuery('#meter' + currentPosition).hide();
+        if (currentPosition > 0) 
+        {
+            currentPosition--;
+        }
+        if (currentPosition < 4)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby1').show();
+        }
+        if (currentPosition < 7 && currentPosition > 3)
+        {
+            jQuery('#baby1').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby2').show();
+        }
+        if (currentPosition < 10 && currentPosition > 6)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby1').hide();
+            jQuery('#baby4').hide();
+            jQuery('#baby3').show();
+        }
+        if (currentPosition > 9)
+        {
+            jQuery('#baby2').hide();
+            jQuery('#baby3').hide();
+            jQuery('#baby1').hide();
+            jQuery('#baby4').show();
+        }
+        jQuery('#meter' + currentPosition).show();
+    }
+
+    jQuery('table').click(function() {
+        increase();
+    });
 });
-
 
