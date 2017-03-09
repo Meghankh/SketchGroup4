f5a5ea03b3acdebb682740dc8acee43bda090cf7
Meghan Hamannwright <meghankh@vt.edu>
2017-03-08 20:12:48 -0700

    display babies

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
 
 jQuery('document').ready(function() {
-    alert('test');
+    jQuery('#baby1').click(function() {
+        jQuery('#baby1').hide();
+        jQuery('#baby2').show();
     });
+    jQuery('#baby2').click(function() {
+        jQuery('#baby2').hide();
+        jQuery('#baby3').show();
+    });
+    jQuery('#baby3').click(function() {
+        jQuery('#baby3').hide();
+        jQuery('#baby4').show();
+    });
+    jQuery('#baby4').click(function() {
+        jQuery('#baby4').hide();
+        jQuery('#baby1').show();
+    });
+
+});
+
+
