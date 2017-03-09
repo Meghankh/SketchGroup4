d51c67b592279b293a39e004e5e18899ff72686f
Meghan Hamannwright <meghankh@vt.edu>
2017-03-08 20:47:36 -0700

    added meter

--- js/index.js
+++ js/index.js
@@ -3,18 +3,26 @@ jQuery('document').ready(function() {
     jQuery('#baby1').click(function() {
         jQuery('#baby1').hide();
         jQuery('#baby2').show();
+        jQuery('#meter1').hide();
+        jQuery('#meter2').show();
     });
     jQuery('#baby2').click(function() {
         jQuery('#baby2').hide();
         jQuery('#baby3').show();
+        jQuery('#meter2').hide();
+        jQuery('#meter3').show();
     });
     jQuery('#baby3').click(function() {
         jQuery('#baby3').hide();
         jQuery('#baby4').show();
+        jQuery('#meter3').hide();
+        jQuery('#meter4').show();
     });
     jQuery('#baby4').click(function() {
         jQuery('#baby4').hide();
         jQuery('#baby1').show();
+        jQuery('#meter4').hide();
+        jQuery('#meter1').show();
     });
 
 });
